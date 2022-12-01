ed20b2beaff2046386acca170dcd8d3081199656
Razak <addorazak8@gmail.com>
2022-12-01 19:29:45 +0100

    day4

---
+++ treasure-map.py
@@ -0,0 +1,16 @@
+row1 = ["⬜️", "️⬜️", "️⬜️"]
+row2 = ["⬜️", "⬜️", "️⬜️"]
+row3 = ["⬜️️", "⬜️️", "⬜️️"]
+map = [row1, row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure? ")
+
+vertical = int(position[0])
+horizontal = int(position[1])
+
+selected_row = map[horizontal - 1]
+selected_row[vertical - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
